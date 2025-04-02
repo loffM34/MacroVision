8a776bfbdede57907910706f294c95340de31d86
Michael <loffm300334@gmail.com>
2025-04-02 14:49:59 -0400

    fixed MacroVision and updated UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "MacroVision",
+  description: "Food analysis with Next.js and Supabase",
 };
 
 const geistSans = Geist({
@@ -36,27 +36,24 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>MacroVision</Link>
-                    <div className="flex items-center gap-2"></div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
+          <div className="min-h-screen flex flex-col">
+            <nav className="border-b border-foreground/10 h-16 flex justify-center">
+              <div className="max-w-6xl w-full px-6 flex items-center justify-between">
+                <Link href="/" className="font-semibold">
+                  MacroVision
+                </Link>
+                {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
               </div>
+            </nav>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p></p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
+            <main className="flex-grow w-full max-w-100xl mx-auto px-6 py-10">
+              {children}
+            </main>
+
+            <footer className="border-t border-foreground/10 py-4 flex justify-center">
+              <ThemeSwitcher />
+            </footer>
+          </div>
         </ThemeProvider>
       </body>
     </html>
